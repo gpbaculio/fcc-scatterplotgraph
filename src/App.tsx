0019728b4abda03c6fd5e2bfa2c372293bced94d
Glendon Philipp <gpbaculio@gmail.com>
2019-05-03 23:11:46 +0800

    x axis ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import * as d3 from 'd3';
 import { url } from './constants';
-
-declare module 'axios' {
-  export interface AxiosResponse<T = any> extends Promise<T> {}
-}
-
+import './App.css';
 interface Details {
   Doping: string;
   Name: string;
@@ -35,16 +32,9 @@ class App extends Component<AppProps, AppState> {
     try {
       const data = await axios.get(url).then(({ data }) =>
         data.map((d: Details) => {
-          const parsedTime = (d.Time as string).split(':');
+          const [minutes, seconds] = (d.Time as string).split(':');
           const Time = new Date(
-            Date.UTC(
-              1970,
-              0,
-              1,
-              0,
-              Number(parsedTime[0]),
-              Number(parsedTime[1])
-            )
+            Date.UTC(1970, 0, 1, 0, Number(minutes), Number(seconds))
           );
           return {
             ...d,
@@ -53,14 +43,48 @@ class App extends Component<AppProps, AppState> {
           };
         })
       );
-      this.setState({ data });
+      this.setState({ data }, () => this.createChart());
     } catch (error) {
       this.setState({ error });
     }
   };
+  createChart = () => {
+    const { data } = this.state;
+    const width = 900,
+      height = 600;
 
+    // x-axis
+    const x = d3
+      .scaleLinear()
+      .domain([
+        d3.min(data, ({ Year }) => Year - 1) as number,
+        d3.max(data, ({ Year }) => Year + 1) as number
+      ])
+      .range([0, width]);
+
+    const xAxis = d3.axisBottom(x).tickFormat(d3.format('d'));
+
+    var svg = d3
+      .select('.graph-container')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    svg
+      .append('g')
+      .attr('class', 'x axis')
+      .attr('id', 'x-axis')
+      .attr('transform', 'translate(60,400)')
+      .call(xAxis)
+      .append('text')
+      .attr('class', 'x-axis-label')
+      .attr('x', width)
+      .attr('y', -6)
+      .style('text-anchor', 'end')
+      .text('Year');
+  };
   render() {
-    return <div className='scatterplotgraph-container' />;
+    return <div className='graph-container' />;
   }
 }
 
